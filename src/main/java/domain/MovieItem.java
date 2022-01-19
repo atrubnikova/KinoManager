d80e9f9f2046a61e15c93b358dafef967dd8335a
atrubnikova <sashk@ngs.ru>
2022-01-19 21:26:49 +0200

    Initial commit

---
+++ src/main/java/domain/MovieItem.java
@@ -0,0 +1,23 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class MovieItem {
+    private int id;
+    private int movieId;
+    private String movieName;
+    private String movieGenre;
+    private int releaseDate;
+
+    public MovieItem(int id) {
+        this.id = id;
+    }
+}
+
